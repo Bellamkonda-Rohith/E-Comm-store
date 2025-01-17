c9e39c5b82eff3471012b706d5499955465cb660
Bellamkonda Rohith <rohtihbellamkonda0@gmail.com>
2025-01-17 12:10:02 +0530

    created the filter through category

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,7 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
-import { store } from './Store/Store.jsx'
+import { store } from './Redux/Store/store.jsx'
 
 import { Provider } from 'react-redux'
 import { StrictMode } from 'react'
